817056affa913e90fc0fe51ad64d9c193cf79d8b
Adel Senhadji <adelsenhadjii@gmail.com>
2021-01-02 13:21:36 +0100

    init bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './App';
 
 ReactDOM.render(
